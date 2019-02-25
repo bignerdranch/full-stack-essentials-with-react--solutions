486ce9d808c3783282c9e527a28137dd44e421e4
jasonreece <jason_reece@apple.com>
2019-02-25 07:43:29 -0500

    adds solution for chapter 26

---
+++ front-end-dev-book/pongboard/src/Game.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Player from './Player';
+
+const Game = (props) => {
+  const playerListItems = props.gameData.map(player => (
+    <Player
+      key={player.id}
+      player={player}
+      updateScore={() => props.updateScore()}
+    />
+  ));
+  return (
+    <ul>
+      { playerListItems }
+    </ul>
+  );
+};
+
+export default Game;
